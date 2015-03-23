9ea688d6ca8c8b74101a456a4ba0bc2ce2cc83ac
PoisonedMinds <steven_biro@hotmail.com>
2015-03-23 09:26:54 -0400

    commenting

--- RandomQuotes/src/randomquotes/RandomQuotes.java
+++ RandomQuotes/src/randomquotes/RandomQuotes.java
@@ -11,17 +11,18 @@ import javax.swing.*;
  * @author Steven Biro
  * @teacher Mr. J. Carron
  * @date 10-Mar-2015 10:47:35 AM
- * @purpose The purpose of this program is to
+ * @purpose The purpose of this program is to provide a random quote from a text
+ * file
  */
 public class RandomQuotes {
-    
+
     public static void main(String[] args) {
-        String quotes[] = new String[50], line;
-        int test = 0, next = 0;
-        try {
+        String quotes[] = new String[50], line;//initalize varibles for quotes
+        int test = 0, next;
+        try {//start buffered reader and catch file not found and io exceptions
             BufferedReader br = new BufferedReader(
                     new FileReader("quotes.txt"));
-            
+
             while ((line = br.readLine()) != null) {
                 quotes[test] = line;
                 test++;
@@ -29,18 +30,19 @@ public class RandomQuotes {
         } catch (FileNotFoundException ex) {
             System.out.println("File not found");
             System.exit(0);
-            
+
         } catch (IOException ex) {
             System.out.println("An error has occured");
             System.exit(1);
         }
         do {
-            next = 1;
+            //select a random number between 0 and 49 then display the quote at that index
             test = (int) (Math.random() * 50);
             JOptionPane.showMessageDialog(null, quotes[test]);
+            //ask if they want to see the next quote
             next = JOptionPane.showConfirmDialog(null, "Do you want to see the next quote?");
         } while (next == 0);
         System.exit(2);
     }
-    
+
 }
